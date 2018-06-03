128359e8aa3812e8eaa22e4195e728da0934a9fc
Leo <leonard.drici@gmail.com>
2018-06-03 13:47:22 +0200

    daada

--- res/payments.php
+++ res/payments.php
@@ -2,8 +2,8 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
-  require "res/database.php";
-  require "res/sec.php";
+  require "database.php";
+  require "sec.php";
   session_start();
   $db = $_SESSION['db'];
   $db->updatePayment(Encryption::encrypt($_POST['email']));
