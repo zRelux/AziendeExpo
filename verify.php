8f0cd4845651eac1d9eaa7ab560549d246e01bb4
Leo <leonard.drici@gmail.com>
2018-06-03 11:03:14 +0200

    Mod

--- verify.php
+++ verify.php
@@ -7,10 +7,10 @@
   session_start();
   if(isset($_GET['email']) && !empty($_GET['email']) && isset($_GET['hash']) && !empty($_GET['hash'])){
       $db = $_SESSION['db'];
-      if($_GET['hash'] == $_SESSION['hash'] && $db->checkEmail(Encryption::crypt($_GET['email'])) != false ){
-        $db->addAzienda(Encryption::crypt($_GET['email']));
+      if($_GET['hash'] == $_SESSION['hash'] && $db->checkEmail(Encryption::encrypt($_GET['email'])) != false ){
+        $db->addAzienda(Encryption::encrypt($_GET['email']));
         $_SESSION['active'] = 1;
-        $db->setActive(Encryption::crypt($_GET['email']));
+        $db->setActive(Encryption::encrypt($_GET['email']));
         header('location: profile.php');
       }else{
         header('location: index.php');
