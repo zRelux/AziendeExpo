7e0544fce31a15b4eac6f99c4228be4104dec44a
Leo <leonard.drici@gmail.com>
2018-06-03 13:45:31 +0200

    adad

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
+
   include('res/database.php');
   session_start();
   if(!isset($_SESSION['db'])){
