01c2c5e53a14c20db2f1e1002c92f10f60b25918
Leo <leonard.drici@gmail.com>
2018-06-03 18:47:50 +0200

    dadad

--- profile.php
+++ profile.php
@@ -102,8 +102,6 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="css/materialize.min.css" rel="stylesheet">
     <link href="css/stileProfilo.css" type="text/css" rel="stylesheet" media="screen,projection" />
-    <link href="css/modal.css" type="text/css" rel="stylesheet" media="screen,projection" />
-
 
   </head>
 
@@ -173,18 +171,6 @@
         <div id="main">
           <div class="wrapper">
             <section id="content">
-              <!-- The Modal -->
-              <div id="myModal" class="modal">
-
-              <!-- The Close Button -->
-              <span class="close">&times;</span>
-
-              <!-- Modal Content (The Image) -->
-              <img class="modal-content" id="img01">
-
-              <!-- Modal Caption (Image Text) -->
-              <div id="caption"></div>
-              </div>
               <div id="profile-page" class="section">
                 <div class="card">
                   <div class="card-image">
@@ -488,28 +474,6 @@
       <script src="js/materialize.min.js"></script>
       <script src="js/main.js"></script>
       <script src="js/sweetalert.min.js"></script>
-      <script type="text/javascript">
-      // Get the modal
-      var modal = document.getElementById('myModal');
-
-      // Get the image and insert it inside the modal - use its "alt" text as a caption
-      var img = document.getElementById('myImg');
-      var modalImg = document.getElementById("img01");
-      var captionText = document.getElementById("caption");
-      img.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
-
-      // Get the <span> element that closes the modal
-      var span = document.getElementsByClassName("close")[0];
-
-      // When the user clicks on <span> (x), close the modal
-      span.onclick = function() {
-      modal.style.display = "none";
-      }
-      </script>
       <footer class="page-footer blue lighten-1">
         <div class="container">
           <div class="row center">
