d30369f149d734ce8147e0bc019e30a30b5038e6
Leo <leonard.drici@gmail.com>
2018-06-12 17:44:10 +0200

    dad

--- index.php
+++ index.php
@@ -305,7 +305,8 @@
     <script src="js/main.js"></script>
     <?php
     if(!isset($_COOKIE['policy']))
-    echo "<script>  var toastHTML = '<span>Visitando questo sito accetti l\'utilizzo dei cookie.</span><br><button class='btn-flat toast-action'><i class='large material-icons'>check</i></button>';
+    echo "<script>
+          var toastHTML = '<span>Visitando questo sito accetti l'utilizzo dei cookie.</span><br><button class='btn-flat toast-action'><i class='large material-icons'>check</i></button>';
           M.toast({html: toastHTML, completeCallback: function(){
             var data = {
               dati: 1,
