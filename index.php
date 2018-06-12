16aad428fb126baef38553c6bd36cf15d972f273
Leo <leonard.drici@gmail.com>
2018-06-12 17:57:09 +0200

    Mah

--- index.php
+++ index.php
@@ -313,16 +313,17 @@
           }});
     </script>
     <?php
-    if(!isset($_COOKIE['policy']))
-      /*echo '<script>
-            var toastHTML = "<span>Visitando questo sito accetti l\'utilizzo dei cookie.</span><br><button class="btn-flat toast-action"><i class="large material-icons">check</i></button>";
-            M.toast({html: toastHTML, completeCallback: function(){
-              var data = {
-                dati: 1,
-              };
-              $.post("res/cookie.php", data);
-            }});
-      </script>';*/
+      if(!isset($_COOKIE['policy'])){
+        /*echo '<script>
+              var toastHTML = "<span>Visitando questo sito accetti l\'utilizzo dei cookie.</span><br><button class="btn-flat toast-action"><i class="large material-icons">check</i></button>";
+              M.toast({html: toastHTML, completeCallback: function(){
+                var data = {
+                  dati: 1,
+                };
+                $.post("res/cookie.php", data);
+              }});
+        </script>';*/
+      }
     ?>
 
 
