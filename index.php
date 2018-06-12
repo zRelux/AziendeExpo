2f1b8377668d6b835addbfa26a8b813e09d165c3
Leo <leonard.drici@gmail.com>
2018-06-12 18:03:36 +0200

    dada

--- index.php
+++ index.php
@@ -303,21 +303,17 @@
     <script src="js/jquery.min.js"></script>
     <script src="js/materialize.min.js"></script>
     <script src="js/main.js"></script>
-    
-    <?php
-      if(!isset($_COOKIE['policy'])){
-        echo '<script>
-              var toastHTML = "<span>Visitando questo sito accetti l\'utilizzo dei cookie.</span><br><button class=''btn-flat toast-action''><i class=''large material-icons''>check</i></button>";
-              M.toast({html: toastHTML, completeCallback: function(){
-                var data = {
-                  dati: 1,
-                };
-                $.post("res/cookie.php", data);
-              }});
-        </script>';
-      }
-    ?>
-
+    <script type="text/javascript">
+    if (document.cookie.indexOf("policy=") < 0) {
+      var toastHTML = "<span>Visitando questo sito accetti l\'utilizzo dei cookie.</span><br><button class='btn-flat toast-action'><i class='large material-icons'>check</i></button>";
+      M.toast({html: toastHTML, completeCallback: function(){
+        var data = {
+          dati: 1,
+        };
+        $.post("res/cookie.php", data);
+      }});
+    }
+    </script>
 
     <footer class="page-footer blue lighten-1">
       <div class="container">
