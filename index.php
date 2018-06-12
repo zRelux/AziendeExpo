ba03534137eb1506c418744cd62afe03715ceb7f
Leo <leonard.drici@gmail.com>
2018-06-12 17:55:26 +0200

    a

--- index.php
+++ index.php
@@ -303,10 +303,7 @@
     <script src="js/jquery.min.js"></script>
     <script src="js/materialize.min.js"></script>
     <script src="js/main.js"></script>
-
-    <?php
-    if(!isset($_COOKIE['policy']))
-    echo '<script>
+    <script>
           var toastHTML = "<span>Visitando questo sito accetti l\'utilizzo dei cookie.</span><br><button class="btn-flat toast-action"><i class="large material-icons">check</i></button>";
           M.toast({html: toastHTML, completeCallback: function(){
             var data = {
@@ -314,7 +311,18 @@
             };
             $.post("res/cookie.php", data);
           }});
-    </script>';
+    </script>
+    <?php
+    if(!isset($_COOKIE['policy']))
+      /*echo '<script>
+            var toastHTML = "<span>Visitando questo sito accetti l\'utilizzo dei cookie.</span><br><button class="btn-flat toast-action"><i class="large material-icons">check</i></button>";
+            M.toast({html: toastHTML, completeCallback: function(){
+              var data = {
+                dati: 1,
+              };
+              $.post("res/cookie.php", data);
+            }});
+      </script>';*/
     ?>
 
 
